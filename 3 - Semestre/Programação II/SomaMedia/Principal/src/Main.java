f02b9c5033dd0eaa815b5dd4c40d4834e3ee321b
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2026-04-06 21:00:03 -0300

    SomaTotal - DONE

--- 3 - Semestre/Programação II/SomaMedia/Principal/src/Main.java
+++ 3 - Semestre/Programação II/SomaMedia/Principal/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int[] numeros;
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        int qtdNumeros = Teclado.leInt("Escreva a quantidade de números para a conta: ");
+        int somaTotal  = 0;
+        int media      = 0;
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < qtdNumeros; i++) {
+            int numero = Teclado.leInt("Escreva um número: ");
 
+            somaTotal += numero;
         }
+
+        media = somaTotal / qtdNumeros;
+
+        System.out.println ("A soma total é: " + somaTotal );
+        System.out.println ("A média é: "      + media );
+
     }
 }
